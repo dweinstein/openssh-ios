0c61fcf6182be3a3a25bb82f486600d67ede75fd
David Weinstein <dweinst@insitusec.com>
2017-08-06 17:22:27 -0400

    Fix up sed for builtin macos ver

--- build-ios.sh
+++ build-ios.sh
@@ -2,16 +2,9 @@
 
 [ ! -f ./openssh-portable/config.h ] && sh -x ./configure-for-ios.sh
 
-case $(md5 ./openssh-portable/sftp-server.c) in
-  *5cfcb0a3813ff7993024d026c542669b*)
-    sed -i 's/Handle/SHandle/g' ./openssh-portable/sftp-server.c
-    break
-    ;;
- *)
-   echo "skipping sftp-server.c patch"
-   break
-   ;;
-esac
+# patch sftp-server Handle (conflicting with MacTypes.h)
+[ ! -f ./openssh-portable/sftp-server.c.bak ] && \
+  sed -i.bak 's/Handle/SHandle/g' ./openssh-portable/sftp-server.c
 
 NPROCS=$(sysctl hw.ncpu | awk '{print $2}')
 . ./ios-env.sh
